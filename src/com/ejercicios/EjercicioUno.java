61786e67d718fba0f341c85f6020fff9cdd09d47
AngeloS22 <z4teria_1209_95@hotmail.com>
2023-02-24 00:05:23 -0500

    Openbootcamp

---
+++ src/com/ejercicios/EjercicioUno.java
@@ -0,0 +1,17 @@
+package com.ejercicios;
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+        String nombre = "Angelo";
+        Integer edad = 27;
+        Double talla = 1.80d;
+        Boolean verdad = true;
+        Long salario = 20000l;
+
+        System.out.println("Su nombre es: " + nombre);
+        System.out.println("Su edad es: " + edad);
+        System.out.println("Su talla es: " + talla);
+        System.out.println("¿Es soltero? " + verdad);
+        System.out.println("Su salario es: " + salario);
+    }
+}
